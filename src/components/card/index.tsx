72603d9e85be2014a9c9476853a1d44a294410f0
Leonam Silva <leonam@nexti.com>
2022-08-25 13:40:49 -0300

    Adicionando função para deletar taske count de tasks e tasks concluídas

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -3,35 +3,47 @@ import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 interface CardProps {
-    content: string
-    done: boolean
+    id: string
+    content: string;
+    done: boolean;
+    onDelete: (card: string) => void;
+    onDone: (checked: boolean, id: string) => void;
 }
 
-export function Card({content, done}: CardProps) {
+export function Card({id, content, done, onDelete, onDone}: CardProps) {
     const [isChecked, setIsChecked] = useState(done);
-    const [doneState, setDoneState] = useState(false)
+    const [doneState, setDoneState] = useState(false);
 
+    function handleDeleteCard() {
+        onDelete(id)
+    }
 
     useEffect(() => {
         isChecked ? setDoneState(true) : setDoneState(false)
+        isChecked ? onDone(true, id) : onDone(false, id)
     }, [isChecked])
 
     return (
-        <div className={styles.card}>
+        <div
+            className={[ styles.card, styles.cardDone].join(" ")}
+            style={{ border: doneState ? "none" : "1px solid var(--gray-400)"}}
+        >
             <div className={styles.checkbox}>
                 <input
-                    id="checkbox"
+                    id={id}
                     type="checkbox"
                     checked={isChecked}
                     onChange={e => setIsChecked(e.target.checked)}
                 />
-                <label htmlFor="checkbox">
+                <label htmlFor={id}>
                     <p className={doneState ? styles.taskDone : ""}>
                         {content}
                     </p>
                 </label>
             </div>
-            <button>
+            <button
+                onClick={handleDeleteCard}
+            >
                 <Trash size={24} />
             </button>
         </div>
--- src/components/taskBoard/index.tsx
+++ src/components/taskBoard/index.tsx
@@ -2,44 +2,77 @@ import styles from "./styles.module.css";
 import { EmptyFrame } from "../emptyFrame";
 import { Card } from "../card";
 import { v4 as uuid } from 'uuid';
+import { useEffect, useState } from "react";
 
 
 const cards = [{
     id: uuid(),
-    content: "Teste novo card",
+    content: "Teste novo card 1",
     done: true
 }, {
     id: uuid(),
-    content: "Teste novo card",
+    content: "Teste novo card 2",
     done: false
 }, {
     id: uuid(),
-    content: "Teste novo card",
+    content: "Teste novo card 3",
     done: false
 },
 ]
 
 export function TaskBoard() {
+    const [taskList, setTaskList] = useState(cards);
+    const [tasksDone, setTasksDone] = useState(0);
+
+    function deleteTask(taskIdToDelete: string) {
+        const taskWithoutDeleteOne = taskList.filter(task => {
+            return task.id !== taskIdToDelete;
+        })
+        setTaskList(taskWithoutDeleteOne);
+    }
+
+    function handleCheckTask(done: boolean, id: string) {
+        taskList.map(task => {
+            if (task.id === id) {
+                task.done = done
+            }
+            setTaskList(taskList)
+        })
+
+        const countTasksDone = taskList.filter(task => {
+            return(
+                task.done === true 
+            )
+        })
+        
+        setTasksDone(countTasksDone.length)
+
+        console.log(taskList);
+    }
+
     return (
         <div className={styles.taskBoard}>
             <div className={styles.countTasks}>
                 <div className={styles.created}>
                     <strong>Tarefas criadas</strong>
-                    <p>{cards.length}</p>
+                    <p>{taskList.length}</p>
                 </div>
                 <div className={styles.done}>
                     <strong>Concluídas</strong>
-                    <p>0</p>
+                    <p>{taskList.length ? `${tasksDone} de ${taskList.length}` : '0'}</p>
                 </div>
             </div>
-            {cards.length ?
+            {taskList.length ?
                 <div className={styles.filledFrame}>
-                    {cards.map(card => {
+                    {taskList.map(task => {
                         return (
                             <Card
-                                key={card.id}
-                                content={card.content}
-                                done={card.done}
+                                key={task.id}
+                                id={task.id}
+                                content={task.content}
+                                done={task.done}
+                                onDelete={deleteTask}
+                                onDone={handleCheckTask}
                             />
                         )
                     })}
